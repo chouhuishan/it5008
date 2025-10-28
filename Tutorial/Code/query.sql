23954044dd19fb33248aa481d36eae05bc28b935
Chou Hui Shan <huishaan@live.com>
2025-10-28 18:34:54 +0800

    revision for sql

--- Tutorial/Code/query.sql
+++ Tutorial/Code/query.sql
@@ -6,5 +6,7 @@ SELECT *
 FROM copy;
 SELECT *
 FROM loan;
+SELECT * -- added due to tutorial 1
+FROM department;
 -- get number of students
 SELECT COUNT(*) AS number_students;
--- Tutorial/tutorial_04/tutorial_4_query.sql
+++ Tutorial/tutorial_04/tutorial_4_query.sql
@@ -9,9 +9,21 @@ WHERE l.borrower = s1.email
     AND l.owner = s2.email
     AND s1.department = s2.department;
 -- ANS: 335
+;
 -- (b): For each faculty, print the number of loans that involve an owner and a borrower from
 -- this faculty?
-SELECT COUNT(*)
-FROM loan l,
+SELECT COUNT(*),
+    d1.faculty
+FROM department d1,
+    department d2,
     student s1,
-    student s2
+    student s2,
+    loan l
+WHERE s1.email = l.owner
+    AND s2.email = l.borrower
+    AND s1.department = d1.department
+    AND s2.department = d2.department
+    AND d1.faculty = d2.faculty
+GROUP BY d1.faculty -- because d1.faculty is used in SELECT(), and COUNT() is an aggregate function, GROUP BY needs to be used
+    -- (c) : What are the average and the standard deviation [3] of the duration of a loan in days?
+    SELE
